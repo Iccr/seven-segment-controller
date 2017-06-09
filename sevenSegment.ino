bbce9464cc93eeae5937113434d8684eb03586e5
ccr <sis.ccr@gmail.com>
2017-06-09 16:35:32 +0545

    corrected the guard condition

--- sevenSegment.ino
+++ sevenSegment.ino
@@ -14,7 +14,7 @@ const int a = 2, b= 3, c= 4, d =5;
 SegmentController segmentController(a,b,c,d);
 
 void setup() {
-
+  // initial setup here.
   }
 
   void loop() {
@@ -23,7 +23,7 @@ void setup() {
         segmentController.show(number);
         delay(1000);
         number++;
-        if(number < 9) {
+        if(number > 9) {
             number = 0;
           }
       }
