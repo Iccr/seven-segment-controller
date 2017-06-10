f2e512461c4c97a323a7512a484c4616bd68acec
ccr <sis.ccr@gmail.com>
2017-06-10 09:34:36 +0545

    moved the private function to .cpp file

--- SegmentController.cpp
+++ SegmentController.cpp
@@ -1,28 +1,98 @@
 #include "SegmentController.h"
 #include "Arduino.h"
 SegmentController::SegmentController(int inputA, int inputB, int inputC, int inputD) {
-    a = inputA;
-    b = inputB;
-    c = inputC;
-    d = inputD;
-    pinMode(a, OUTPUT);
-    pinMode(b, OUTPUT);
-    pinMode(c, OUTPUT);
-    pinMode(d, OUTPUT);
+  a = inputA;
+  b = inputB;
+  c = inputC;
+  d = inputD;
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
 }
 
 void SegmentController::show(int number) {
-    switch(number) {
-        case 0: zero(); break;
-        case 1: one(); break;
-        case 2: two(); break;
-        case 3: three(); break;
-        case 4: four(); break;
-        case 5: five(); break;
-        case 6: six(); break;
-        case 7: seven(); break;
-        case 8: eight(); break; 
-        case 9: nine(); break; 
-      }
+  switch (number) {
+    case 0: zero(); break;
+    case 1: one(); break;
+    case 2: two(); break;
+    case 3: three(); break;
+    case 4: four(); break;
+    case 5: five(); break;
+    case 6: six(); break;
+    case 7: seven(); break;
+    case 8: eight(); break;
+    case 9: nine(); break;
   }
+}
+
+void SegmentController::zero() {
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::one() {
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::two() {
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::three() {
+  digitalWrite(a, 1);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::four() {
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::five() {
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::six() {
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::seven() {
+  digitalWrite(a, 1);
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+}
+
+void SegmentController::eight() {
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+}
+void SegmentController::nine() {
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+}
+
 
--- SegmentController.h
+++ SegmentController.h
@@ -11,76 +11,16 @@ class SegmentController {
 private:
   // input pins
   int a, b, c, d;
-  void zero() {
-    digitalWrite(a, 0);
-    digitalWrite(b, 0);
-    digitalWrite(c, 0);
-    digitalWrite(d, 0);
-  }
-
-void one() {
-    digitalWrite(a, 1);
-    digitalWrite(b, 0);
-    digitalWrite(c, 0);
-    digitalWrite(d, 0);
-  }
-
-  void two() {
-    digitalWrite(a, 0);
-    digitalWrite(b, 1);
-    digitalWrite(c, 0);
-    digitalWrite(d, 0);
-    }
-
-    void three() {
-      digitalWrite(a, 1);
-      digitalWrite(b, 1);
-      digitalWrite(c, 0);
-      digitalWrite(d, 0);
-      }
-
-     void four() {
-      digitalWrite(a, 0);
-      digitalWrite(b, 0);
-      digitalWrite(c, 1);
-      digitalWrite(d, 0);
-      }
-
-      void five() {
-      digitalWrite(a, 1);
-      digitalWrite(b, 0);
-      digitalWrite(c, 1);
-      digitalWrite(d, 0);
-      }
-
-      void six() {
-      digitalWrite(a, 0);
-      digitalWrite(b, 1);
-      digitalWrite(c, 1);
-      digitalWrite(d, 0);
-      }
-
-      void seven() {
-      digitalWrite(a, 1);
-      digitalWrite(b, 1);
-      digitalWrite(c, 1);
-      digitalWrite(d, 0);
-      }
-
-      void eight() {
-      digitalWrite(a, 0);
-      digitalWrite(b, 0);
-      digitalWrite(c, 0);
-      digitalWrite(d, 1);
-      }
-
-      void nine() {
-      digitalWrite(a, 1);
-      digitalWrite(b, 0);
-      digitalWrite(c, 0);
-      digitalWrite(d, 1);
-      }
-  
+  void zero();
+  void one();
+  void two();
+  void three();
+  void four();  
+  void five();
+  void six();
+  void seven();
+  void eight();
+  void nine();
 };
 
 #endif
